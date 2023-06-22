b0fde1d6d9e7595d487dd279d72ccc7556e70001
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2023-06-22 09:44:43 -0300

    Requisitos de testes

--- app/backend/src/tests/Team.test.ts
+++ app/backend/src/tests/Team.test.ts
@@ -4,7 +4,7 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-// import Example from '../database/models/ExampleModel';
+
 import SequelizeTeam from '../database/models/SequelizeTeam';
 import TeamMock from './mocks/Team.Mock';
 
@@ -15,32 +15,6 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Testa os times', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
-
-  // let chaiHttpResponse: Response;
-
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
   it('Testa se ao fazer um get para a rota /teams ele retorna todos os times cadastrados', async () => {
     sinon.stub(SequelizeTeam, 'findAll').resolves(TeamMock.teams as any);
     const { status, body } = await chai.request(app).get('/teams');
